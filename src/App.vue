51ff2b13fc7f48e21c5c455d5b18f3b4e3ac5d32
Htoo Wai Yan <htoowaiyan.dev@gmail.com>
2024-03-06 18:01:26 +0630

    Divide components and use them in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <MyHeader />
   <div class="container">
-    <MyBalance />
+    <MyBalance :total="total" />
     <IncomeExpense />
-    <TranscationList />
+    <TranscationList :transactions="transactions"/>
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import MyHeader from '@/components/MyHeader.vue';
 import MyBalance from '@/components/MyBalance.vue';
 import IncomeExpense from '@/components/IncomeExpense.vue';
 import TranscationList from '@/components/TranscationList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-export default {
-  components: {
-    MyHeader,
-    MyBalance,
-    IncomeExpense,
-    TranscationList,
-    AddTransaction,
-  },
-};
+import { ref,computed } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: "Flowers", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 300.00 },
+  { id: 3, text: "Book", amount: -10.00 },
+  { id: 4, text: "Laptop", amount: 150.00 }
+])
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, item) => (acc += item.amount), 0).toFixed(2);
+});
+
 </script>
 
 <style lang="scss">
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,14 +1,15 @@
 /* eslint-disable */
+
 <template>
     <h3>Add new transaction</h3>
     <form id="form">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..."  />
+            <input type="text" id="text" placeholder="Enter text..." />
         </div>
         <div class="form-control">
             <label for="amount">Amount <br />
-                Blah</label>
+                (negative - expense, positive - income)</label>
             <input type="text" id="amount" placeholder="Enter amount..." />
         </div>
         <button class="btn">Add transaction</button>
